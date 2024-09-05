70ad7bcf85b4c9b2829e90af1c9693bf703b24a3
dzlek <6894255@gmail.com>
2024-09-05 13:29:30 +0300

    feat : do task

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-function killer(suspectInfo, dead) {
-  for (let suspect in suspectInfo) {
-    if (dead.every((person) => suspectInfo[suspect].includes(person))) {
-      console.log(suspect);
-    }
+function dataReverse(data) {
+  const newArr = [];
+  for (let i = 0; i < data.length; i = i + 8) {
+    newArr.push(data.slice(i, i + 8));
   }
+  const res = newArr.reverse().flat();
+
+  console.log(res);
 }
 
-killer(
-  {
-    James: ["Jacob", "Bill", "Lucas"],
-    Johnny: ["David", "Kyle", "Lucas"],
-    Peter: ["Lucy", "Kyle"],
-  },
-  ["Lucas", "Bill"]
-);
+dataReverse([0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1]);
