f8ab03366ec4620da7a86d6c9047969fb72720b0
dzlek <6894255@gmail.com>
2024-09-08 18:13:50 +0300

    feat: solution

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-function strCount(obj) {
-  let count = 0;
+var Alphabet = {
+  BINARY: "01",
+  OCTAL: "01234567",
+  DECIMAL: "0123456789",
+  HEXA_DECIMAL: "0123456789abcdef",
+  ALPHA_LOWER: "abcdefghijklmnopqrstuvwxyz",
+  ALPHA_UPPER: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  ALPHA: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  ALPHA_NUMERIC:
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+};
 
-  function countString(value) {
-    if (typeof value === "string") count++;
-    if (typeof value === "object" && value !== null) {
-      for (let keys in value) {
-        countString(value[keys]);
-      }
+var bin = Alphabet.BINARY,
+  oct = Alphabet.OCTAL,
+  dec = Alphabet.DECIMAL,
+  hex = Alphabet.HEXA_DECIMAL,
+  allow = Alphabet.ALPHA_LOWER,
+  alup = Alphabet.ALPHA_UPPER,
+  alpha = Alphabet.ALPHA,
+  alnum = Alphabet.ALPHA_NUMERIC;
+
+function convert(input, source, target) {
+  function fromInputTo10(input, source) {
+    let res = 0;
+    for (let i = 0; i < input.length; i++) {
+      res = res * source.length + source.indexOf(input[i]);
     }
+    return res;
   }
-  countString(obj);
 
-  console.log("return " + count);
+  function from10ToTarget(value, target) {
+    if (value === 0) return target[0];
+    let base = target.length;
+    let res = "";
+    while (value > 0) {
+      res = target[value % base] + res;
+      value = Math.floor(value / base);
+    }
+    return res;
+  }
+
+  const base10Value = fromInputTo10(input, source);
+
+  console.log("return " + from10ToTarget(base10Value, target));
 }
 
-strCount({
-  first: "1",
-  second: "2",
-  third: false,
-  fourth: ["anytime", 2, 3, 4],
-  fifth: null,
-  sixth: undefined,
-  seventh: {},
-});
+convert("15", dec, oct);
