ccf0e9b20a7db0c567a3a8a9ae550c8444d3bf42
dzlek <6894255@gmail.com>
2024-09-10 12:36:02 +0300

    feat: solution

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-function getCard() {
-  let res = [];
-
-  function createArray(letter, num, start, end) {
-    let set = new Set();
-    while (set.size < num) {
-      set.add(
-        letter + String(Math.floor(start + Math.random() * (end + 1 - start)))
-      );
-    }
-
-    [...set].map((el) => res.push(el));
-  }
-
-  createArray("B", 5, 1, 15);
-  createArray("I", 5, 16, 30);
-  createArray("N", 4, 31, 45);
-  createArray("G", 5, 46, 60);
-  createArray("O", 5, 61, 75);
-
-  function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-  }
-  shuffle(res);
+var countBits = function (n) {
+  res = n
+    .toString(2)
+    .split("")
+    .reduce((prev, cur) => prev + (cur === "1" ? 1 : 0), 0);
 
   console.log("return " + res);
-}
+};
 
-getCard();
+countBits(7);
