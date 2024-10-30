fe3ab33b017b906f4a73ebf46e7bbb3c6b6a46fc
dzlek <6894255@gmail.com>
2024-10-30 15:05:15 +0300

    fix: minor change

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-function rgb(r, g, b) {
-  const converter = (value) =>
-    (value >= 255 ? 255 : value < 0 ? 0 : value)
-      .toString(16)
-      .padStart(2, "0")
-      .toUpperCase();
-  const res = converter(r) + converter(g) + converter(b);
-  return res;
+function solution(str, ending) {
+  if (ending === "") return true;
+  return str.slice(-ending.length) !== ending ? false : true;
 }
-
-console.log(rgb(300, 255, 255));
-
-// []                                -->  "no one likes this"
-// ["Peter"]                         -->  "Peter likes this"
-// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
-// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
-// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+console.log(solution("abcde", "cde"));
