0287753f495639c4761d5f3a9e435017c2035c28
dzlek <6894255@gmail.com>
2024-11-19 13:16:12 +0300

    init: start stream

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-function toWeirdCase(string) {
-  const res = string
-    .split(" ")
-    .map((word) =>
-      word
-        .split("")
-        .map((letter, index) =>
-          index % 2 === 0 ? letter.toUpperCase() : letter.toLowerCase()
-        )
-        .join("")
-    )
-    .join(" ");
-  return res;
-}
+const arr = [1, 2, 3, 4, 5, 5, 7, 8, 9];
 
-console.log(toWeirdCase("This is a test"));
+const arr2 = new Set(arr);
+
+// arr2.forEach((item, i) => {
+//   console.log(item, i);
+// });
+
+// arr2.map((value, index, array) => {
+//   console.log(value);
+// });
+// for (entries of arr2) {
+//   console.log(entries);
+// }
+
+// const person = {
+//   name: "John",
+//   age: 30,
+//   city: "New York",
+// };
+
+// const { name, age } = person;
+// console.log(age);
+
+console.log(arr.includes(5));
