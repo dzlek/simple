8a89371226a7858f8fb0829b870da29cfad9c84e
dzlek <6894255@gmail.com>
2025-02-28 10:06:04 +0300

    feat: end

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-console.log("logged");
+console.log("start");
 
-console.log("awesome");
-console.log("logged");
+// const myPromise = new Promise((resolve, reject) => resolve(2))
+//   .then((number) => {
+//     console.log(number ** 2);
+//     return number ** 2;
+//   })
+//   .then((number) => console.log(number ** 2));
 
-export function sum(a, b) {
-  return a + b;
-}
+// const squаre = {
+//   side: 10,
+//   ea() {
+//     return this.side * this.side;
+//   },
+//   perimeter: () => 2 * this.side,
+// };
+
+// console.log(squаre.ea()); //100
+// console.log(squаre.perimeter()); //NaN
+
+// console.log(undefined * 2);
+
+let a = 2;
+let b = 3;
+
+const sum = (a, b) => a + b;
+
+console.log(sum(a, b));
